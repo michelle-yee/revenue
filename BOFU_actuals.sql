b584aafad944991258af34a78baf2d5d8692cfd4
michelle-yee <59663553+michelle-yee@users.noreply.github.com>
2022-10-07 13:27:40 -0700

    Create BOFU_actuals.sql

---
+++ BOFU_actuals.sql
@@ -0,0 +1,34 @@
+	with summary as(
+SELECT DISTINCT
+	conversion_date,
+	DATE_TRUNC('month',conversion_date) themonth,
+	bench_id,
+	lead_id,
+	opp_id,
+	channel,
+	conversion_credit,
+	conversion_type,
+	CASE 
+		WHEN channel IN ('other_gp',
+			'display',
+			'competitor',
+			'youtube',
+			'non_branded_search',
+			'branded_search',
+			'podcast',
+			'facebook',
+			'reddit') THEN 'Paid'
+		WHEN channel IN (
+			'email',
+			'search_content',
+			'search_core',
+			'social') THEN 'Inbound'
+		ELSE channel
+	END AS channel1,
+	sfu.name AS rep_name
+FROM revenue.v_mcod_attributions_long_unallocated mcod
+LEFT JOIN salesforce.sf_lead ld ON mcod.lead_id = ld.id
+LEFT JOIN salesforce.sf_user sfu ON ld.ownerid = sfu.id
+WHERE conversion_date >= '2021-01-01')
+
+SELECT DISTINCT channel,channel1 FROM summary;
